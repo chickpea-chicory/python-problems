b3c16d44e6a5a35955115d28710c6cdddd1bf3c0
unknown <nairn036@gmail.com>
2026-07-10 22:07:21 +0100

    initial commit

---
+++ python problems/python_problem.py
@@ -0,0 +1,4 @@
+user_input_age = int(input("whats your age? "))
+user_input_name = input("whats your name? ")
+years_till_100 = 100 - user_input_age
+print(user_input_name, "you will turn 100 in", years_till_100, "years")
